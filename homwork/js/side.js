205746446bceb7a8fc60f312da0124372c1e4d0a
mammadli1994 <49428069+mammadli1994@users.noreply.github.com>
2019-05-16 03:52:09 +0400

    Add files via upload

---
+++ homwork/js/side.js
@@ -0,0 +1,16 @@
+
+
+var div=document.querySelector("#ourSide")
+var button=document.querySelector("#ourSide");
+
+
+function changed(){
+    div.style.marginLeft="-180px";
+    
+}
+
+function close(){
+    div.style.marginRight="180px";
+    
+}
+
